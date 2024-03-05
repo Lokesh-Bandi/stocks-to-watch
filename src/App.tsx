b2694d13273f63bcd3e6883bdcdadce089c78d20
lokesh.bandi <lokesh.bandi@pragmaticplay.com>
2024-03-05 17:51:10 +0530

    Adding prettier to the codebase and linking with the eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import "./styles.css";
-import LOGO from "../assets/logo.png";
-import LOGO_SVG from "../assets/logo1.svg";
+import './styles.css';
+import LOGO from '../assets/logo.png';
+import LOGO_SVG from '../assets/logo1.svg';
 
 export const App = () => {
   return (
     <>
       <h1>
-        React basic template version - 1.0.0 {process.env.NODE_ENV}{" "}
+        React basic template version - 1.0.0 {process.env.NODE_ENV}{' '}
         {process.env.developer}
       </h1>
       <img src={LOGO} alt="logo" height={200} width={200} />
