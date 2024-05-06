de40230b8e8f769ee5de34d6aebde7cb564bb07a
lokesh.bandi <lokesh.bandi@pragmaticplay.com>
2024-05-06 12:13:29 +0530

    Changes to run the applicaiton over a specific port

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -2,6 +2,9 @@ const webpack = require('webpack');
 module.exports = {
   mode: 'development',
   devtool: 'cheap-module-source-map',
+  devServer: {
+    port: 8084,
+  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.developer': JSON.stringify('Lokesh Bandi -dev'),
